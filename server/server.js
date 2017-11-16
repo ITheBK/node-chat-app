abe7eeb6a1ab19c5c6cfd9268de254d0413ae5ef
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-16 11:37:47 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+  const path = require('path');
+  const express = require('express');
+
+  //To not use ../ to go to public/index.html
+  const publicPath = path.join(__dirname, '../public');
+  const port = process.env.PORT || 3000;
+  var app = express();
+
+  app.use(express.static(publicPath));
+
+  app.listen(port, () => {
+
+    console.log(`Server is up on ${{port}}`);
+
+  });
