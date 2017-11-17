f5a77320af6b9e2ab719949e0464081dba6f7745
Bharath Kumar <bharath.kumar@autoninja.in>
2017-11-17 18:07:16 +0530

    Send message to Room specific

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,6 @@
     event.preventDefault();
     var messageTextbox=  $('[name=message]');
     socket.emit('createMessage', {
-      from: 'User',
       text: messageTextbox.val()
     }, function() {
         messageTextbox.val('');
